bdb67687af7a4f4254ed32db9d120f7432a29602
AlvarHHM <mahoihei@gmail.com>
2016-07-25 16:34:17 +0100

    update

--- src/main.c
+++ src/main.c
@@ -26,16 +26,12 @@ void rand_str(char *dest, size_t length) {
 }
 
 void measure_query_time(sqlite3* db, int i, char* field){
-//    clock_t start = clock(), diff;
     double start = getCPUTime(), diff;
     char sql[50];
     sprintf(sql, "select * from fun order by %s", field);
     sqlite3_exec(db, sql, 0, 0, 0);
     diff = getCPUTime() - start;
-    printf("%d\t\t%lf\n", i, diff);
-//    diff = clock() - start;
-//    long msec = diff * 1000 / CLOCKS_PER_SEC;
-//    printf("%d\t%ld\n", i, msec / 1000, msec % 1000);
+    printf("%d\t%lf\n", i, diff);
 }
 
 int main(int argc, char *argv[]) {
@@ -51,9 +47,11 @@ int main(int argc, char *argv[]) {
     sqlite3_exec(db, "drop table if exists fun;", NULL, NULL, NULL);
     sqlite3_exec(db, "create table fun(id num, randstr text, randint num, randreal real);", NULL, NULL, NULL);
     srand(time(NULL));
-    int rows = 10000000;
+    char sql[] = "insert into fun values(?1,?2,?3,?4)";
+    sqlite3_stmt* stmt;
+    sqlite3_prepare_v2(db, sql, strlen(sql), &stmt, NULL);
     sqlite3_exec(db, "BEGIN TRANSACTION;", NULL, NULL, NULL);
-    for (int i = 1; 1==1; i++) {
+    for (int i = 1; i < 2000000000; i++) {
 
         long long int randint = ((long long) rand() << 32) | rand();
         randint = (rand() % 2)? randint: -randint;
@@ -61,15 +59,19 @@ int main(int argc, char *argv[]) {
         int len = randr(1, 11);
         char randstr[len];
         rand_str(randstr, len);
-        char buffer[300];
-        sprintf(buffer, "insert into fun values(%d,'%s',%lld,%lf)",
-                i, randstr, randint, randreal);
-        sqlite3_exec(db, buffer, NULL, NULL, NULL);
-        if (i % atoi(argv[1]) == 0){
-            sqlite3_exec(db, "COMMIT TRANSACTION", NULL, NULL, NULL);
-            measure_query_time(db, i, argv[2]);
-            sqlite3_exec(db, "BEGIN TRANSACTION;", NULL, NULL, NULL);
+        sqlite3_bind_int(stmt, 1, i);
+        sqlite3_bind_text(stmt, 2, randstr, len, SQLITE_STATIC);
+        sqlite3_bind_int64(stmt, 3, randint);
+        sqlite3_bind_double(stmt, 4, randreal);
+        sqlite3_step(stmt);
+        if (argc < 4 || i > atoi(argv[3])){
+            if (i % atoi(argv[1]) == 0){
+                sqlite3_exec(db, "COMMIT TRANSACTION", NULL, NULL, NULL);
+                measure_query_time(db, i, argv[2]);
+                sqlite3_exec(db, "BEGIN TRANSACTION;", NULL, NULL, NULL);
+            }
         }
+        sqlite3_reset(stmt);
 
     }
     sqlite3_exec(db, "COMMIT TRANSACTION", NULL, NULL, NULL);
